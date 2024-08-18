843bcb1023ac06838fd6e155e2fdc1b974f95f48
kalyani-ingle <kalyaniingle@gmail.com>
2024-08-18 17:07:07 +0000

    cors enabled

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const cookieParser = require("cookie-parser");
 const path= require("path");
 const cors = require("cors");
 const apiRoutes = require("./routes/apiRoutes");
+const allowedOrigins = process.env.FRONTEND_URL.split(',');
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL, 
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, 
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
